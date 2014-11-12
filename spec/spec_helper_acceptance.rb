e984f19c02e71bfe1e7ba75078eba097a6209ee1
Trey Dockendorf <treydock@tamu.edu>
2014-11-12 16:59:03 -0600

    Update acceptance tests now that augeasprovider_shellvar bug fixed

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -28,7 +28,7 @@ RSpec.configure do |c|
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'puppetlabs-firewall'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'puppetlabs-inifile'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'herculesteam-augeasproviders_shellvar', '--version', '"<= 2.0.1"'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'herculesteam-augeasproviders_shellvar'), { :acceptable_exit_codes => [0,1] }
     end
   end
 end
