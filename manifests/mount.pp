7d03848c6d8ccebe115b0a87e922103ab07b8110
Trey Dockendorf <treydock@gmail.com>
2015-03-24 15:12:46 -0500

    * Allow nfs::mount to have owner, group and mode set for the mounted directory

--- manifests/mount.pp
+++ manifests/mount.pp
@@ -20,36 +20,46 @@ define nfs::mount (
   $device,
   $ensure           = 'mounted',
   $atboot           = true,
-  $path             = 'UNSET',
+  $path             = $title,
   $options          = 'rw',
   $manage_directory = true,
+  $owner            = undef,
+  $group            = undef,
+  $mode             = undef,
 ) {
 
-  $path_real = $path ? {
-    'UNSET' => $title,
-    default => $path,
-  }
+  validate_bool($manage_directory)
 
   $options_real = is_array($options) ? {
     true    => join($options, ','),
     default => $options,
   }
 
-  if $manage_directory {
-    if ! defined(File[$path_real]) {
-      file { $path_real:
-        ensure => 'directory',
-      }
+  if ! defined(File[$path]) and $manage_directory {
+    exec { "mkdir-${title}":
+      path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+      command => "mkdir -p ${path}",
+      creates => $path,
+      before  => Mount[$title],
+    }
+
+    file { $title:
+      ensure  => 'directory',
+      path    => $path,
+      owner   => $owner,
+      group   => $group,
+      mode    => $mode,
+      require => Mount[$title],
     }
   }
 
   mount { $title:
     ensure  => $ensure,
-    name    => $path_real,
+    name    => $path,
     atboot  => $atboot,
     device  => $device,
     fstype  => 'nfs',
     options => $options_real,
-    require => [ Package['nfs'], File[$path_real] ],
+    require => Package['nfs'],
   }
 }
--- spec/defines/nfs_mount_spec.rb
+++ spec/defines/nfs_mount_spec.rb
@@ -14,17 +14,35 @@ describe 'nfs::mount' do
   let(:default_params) {{ :device => "192.168.1.1:/export/foo" }}
   let(:params) { default_params }
 
-  it { should contain_file('/mnt/foo').with_ensure('directory') }
+  it do
+    should contain_exec('mkdir-/mnt/foo').with({
+      :path     => '/bin:/usr/bin:/sbin:/usr/sbin',
+      :command  => 'mkdir -p /mnt/foo',
+      :creates  => '/mnt/foo',
+      :before   => 'Mount[/mnt/foo]',
+    })
+  end
+
+  it do
+    should contain_file('/mnt/foo').with({
+      :ensure   => 'directory',
+      :path     => '/mnt/foo',
+      :owner    => nil,
+      :group    => nil,
+      :mode     => nil,
+      :require  => 'Mount[/mnt/foo]',
+    })
+  end
 
   it do
     should contain_mount('/mnt/foo').with({
-      'ensure'  => 'mounted',
-      'name'    => '/mnt/foo',
-      'atboot'  => 'true',
-      'device'  => '192.168.1.1:/export/foo',
-      'fstype'  => 'nfs',
-      'options' => 'rw',
-      'require' => [ 'Package[nfs]', 'File[/mnt/foo]' ],
+      :ensure   => 'mounted',
+      :name     => '/mnt/foo',
+      :atboot   => 'true',
+      :device   => '192.168.1.1:/export/foo',
+      :fstype   => 'nfs',
+      :options  => 'rw',
+      :require  => 'Package[nfs]',
     })
   end
 
@@ -42,6 +60,44 @@ describe 'nfs::mount' do
 
   context "when path => '/mnt/foobar'" do
     let(:params) { default_params.merge({ :path => '/mnt/foobar'}) }
+
+    it do
+      should contain_exec('mkdir-/mnt/foo').with({
+        :path     => '/bin:/usr/bin:/sbin:/usr/sbin',
+        :command  => 'mkdir -p /mnt/foobar',
+        :creates  => '/mnt/foobar',
+        :before   => 'Mount[/mnt/foo]',
+      })
+    end
+
+    it do
+      should contain_file('/mnt/foo').with({
+        :ensure   => 'directory',
+        :path     => '/mnt/foobar',
+        :owner    => nil,
+        :group    => nil,
+        :mode     => nil,
+        :require  => 'Mount[/mnt/foo]',
+      })
+    end
+
     it { should contain_mount('/mnt/foo').with_name('/mnt/foobar') }
+  end
+
+  context 'when manage_directory => false' do
+    let(:params) { default_params.merge({ :manage_directory => false }) }
+
+    it { should_not contain_exec('mkdir-/mnt/foo') }
+    it { should_not contain_file('/mnt/foo') }
+  end
+
+  context 'when path is already defined' do
+    let(:params) { default_params.merge({ :path => '/mnt/foobar'}) }
+    let(:pre_condition) do
+      "file { '/mnt/foobar': ensure => directory }"
+    end
+
+    it { should_not contain_exec('mkdir-/mnt/foo') }
+    it { should_not contain_file('/mnt/foo') }
   end
 end
